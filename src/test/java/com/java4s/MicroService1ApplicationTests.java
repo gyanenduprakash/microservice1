753b9f7eb923fa16de1af1de53d792597c2ff911
gyanenduprakash <gyanendu8191@gmail.com>
2023-04-27 22:13:12 +0530

    base bransh

---
+++ src/test/java/com/java4s/MicroService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java4s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
